f20acee4ce2d1d2874f5b2b6e972d4a1e6e0fd3c
boytrend0108 <boytrend@gmail.com>
2025-09-13 10:36:17 +0300

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('Hello DevOps Pipeline!');
+console.log('This is a sample DevOps pipeline script.');
---
+++ test.js
@@ -0,0 +1,7 @@
+console.log('Starting tests for DevOps Pipeline...');
+
+setTimeout(() => {
+  console.log('Waiting for 3 seconds...');
+}, 3000);
+
+console.log('Tests completed.');
